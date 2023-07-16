47385a4d4bca82aa64c4a8a72113086be7eb7bed
Neem48 <65356295+Neem48@users.noreply.github.com>
2023-07-16 11:05:56 +0100

    Update emi calculator.py

--- emi calculator.py
+++ emi calculator.py
@@ -6,5 +6,7 @@ st.title("EMI calculator.")
 p=st.slider("Enter principal amount:",1,10000)
 n=st.slider("Enter time borrowed for, in months:" 1,36)
 r=st.slider("Enter rate of interest:",1,15)
+n=n*12
+r=r/12
 if st.button("calculate"):
 	calculate_emi(p,n,r)
